1da55bfeed2a1e942bc28746d1f438241adc9642
Matjaz Lipus <matjazl@gmail.com>
2012-07-26 14:11:15 +0200

    XMLDynFetcher strict exception

--- XMLDynFetcher.class.php
+++ XMLDynFetcher.class.php
@@ -7,7 +7,7 @@ class XMLDynFetcher extends DynFetcher
     protected function fetch()
     {
         if (!$xml = @file_get_contents($this->url)) {
-            throw new Exception("Missing content at url: {$this->url}");
+            throw new UnexpectedValueException('Error retrieving data.', self::ERROR_RETRIEVE_DATA);
         }
         return simplexml_load_string($xml,
             'SimpleXMLElement', LIBXML_NOCDATA);
